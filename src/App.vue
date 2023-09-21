46bc7beb7145079e26673903cd5ac47053eb6695
Hrćan Jan <xhrcan@stuba.sk>
2023-09-21 10:42:41 +0200

    fix: consistent tabs and spaces

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
 	<h2>Nová položka</h2>
-  <input @keyup.enter="addItem" v-model="input" placeholder="Názov položky">
+	<input @keyup.enter="addItem" v-model="input" placeholder="Názov položky">
 	<button @click="addItem()">Pridať</button>
-  <hr>
-  <h2>Pridané položky</h2>
+	<hr>
+	<h2>Pridané položky</h2>
 	<ul>
 		<li v-for="item in validItems" :key="`item-${item.id}`">
 			<span @click="deleteItem(item)" style="margin-right: 15px" class="delete">X</span>
 			{{  item.text }}
 		</li>
-  </ul>
-  <hr>
-  <h2>Zmazané položky</h2>
-  <ul>
-    <li v-for="item in deletedItems" :key="`item-${item.id}`">
+	</ul>
+	<hr>
+	<h2>Zmazané položky</h2>
+	<ul>
+		<li v-for="item in deletedItems" :key="`item-${item.id}`">
 			<s>{{  item.text }}</s>
 		</li>
 	</ul>
@@ -28,7 +28,7 @@ export default {
 		}
 	},
 
-  methods: {
+	methods: {
 		addItem() {
 			this.items.push({
 				id: this.items.length + 1,
@@ -38,7 +38,7 @@ export default {
 			this.input = ''
 		},
 
-    deleteItem(item) {
+		deleteItem(item) {
 			item.is_deleted = true
 		}
 	},
@@ -57,23 +57,23 @@ export default {
 
 <style>
 body {
-  color: white;
-  background-color: #191919;
+	color: white;
+	background-color: #191919;
 }
 
 button {
-  display: block;
-  margin: 2px 0;
+	display: block;
+	margin: 2px 0;
 }
 
 .delete {
-  cursor: pointer;
+	cursor: pointer;
 }
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	margin-top: 60px;
 }
 </style>
